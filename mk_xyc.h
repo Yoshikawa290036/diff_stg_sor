cce89c84c77f538fdbb70411e651ec004a74584a
Yoshikawa290036 <290036hirokix@gmail.com>
2022-09-17 14:15:49 +0900

    fig mk_xyc

--- mk_xyc.h
+++ mk_xyc.h
@@ -1,6 +1,11 @@
 write(fname,'("xyc",i7.7)') nstep
 open(11,file=fname)
 
+j = 0
+do i = 0,nr-1
+    write(11,'(20e20.10)') xs(i,j),-ys(i,j),c(i,j)
+enddo
+
 do j = 0,ntheta-1
     do i = 0,nr-1
         write(11,*) xs(i,j),ys(i,j),c(i,j)
@@ -8,12 +13,10 @@ do j = 0,ntheta-1
     write(11,'()')
 enddo
 
-! do j = 0,ntheta-1
-!     do i = 0,nr
-!         write(11,'(20e20.10)') c(i,j)
-!     enddo
-!     write(11,'()')
-! enddo
+j = ntheta-1
+do i = 0,nr-1
+    write(11,'(20e20.10)') xs(i,j),-ys(i,j),c(i,j)
+enddo
 
 ! do j = ntheta-1,0,-1
 !     do i = 0,nr-1
